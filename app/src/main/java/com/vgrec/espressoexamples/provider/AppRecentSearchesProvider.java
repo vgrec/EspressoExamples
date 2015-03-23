6684d715adf080065309d9bdd8702af5e2cdd568
vgrec <vgrec@winify.com>
2015-03-23 17:35:36 +0200

    Fixed search suggestions

--- app/src/main/java/com/vgrec/espressoexamples/provider/AppRecentSearchesProvider.java
+++ app/src/main/java/com/vgrec/espressoexamples/provider/AppRecentSearchesProvider.java
@@ -7,7 +7,7 @@ import android.content.SearchRecentSuggestionsProvider;
  */
 public class AppRecentSearchesProvider extends SearchRecentSuggestionsProvider {
 
-    public final static String AUTHORITY = "com.vgrec.espressosamples";
+    public final static String AUTHORITY = "com.vgrec.espressoexamples";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public AppRecentSearchesProvider() {
