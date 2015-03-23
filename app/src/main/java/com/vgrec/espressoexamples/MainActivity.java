d369a6d2493e34caa2214116d354af48ba88340e
vgrec <vgrec@winify.com>
2015-03-23 11:23:57 +0200

    Created ViewPagerActivity

--- app/src/main/java/com/vgrec/espressoexamples/MainActivity.java
+++ app/src/main/java/com/vgrec/espressoexamples/MainActivity.java
@@ -6,9 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,27 +14,20 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Hold button ids with the corresponding Activities to launch, when a button is clicked
-        Map<Integer, Class<? extends Activity>> actions = new HashMap<>();
-
-        actions.put(R.id.type_text_button, EnterNameActivity.class);
-        actions.put(R.id.spinner_selection_button, SpinnerSelectionActivity.class);
-        actions.put(R.id.custom_list_adapter_button, CustomListActivity.class);
-        actions.put(R.id.search_view_button, SearchViewActivity.class);
-        actions.put(R.id.action_bar_button, ActionBarExampleActivity.class);
-
-        setListeners(actions);
+        setListener(R.id.type_text_button, EnterNameActivity.class);
+        setListener(R.id.spinner_selection_button, SpinnerSelectionActivity.class);
+        setListener(R.id.custom_list_adapter_button, CustomListActivity.class);
+        setListener(R.id.search_view_button, SearchViewActivity.class);
+        setListener(R.id.action_bar_button, ActionBarExampleActivity.class);
+        setListener(R.id.viewpager_button, ViewPagerActivity.class);
     }
 
-    private void setListeners(Map<Integer, Class<? extends Activity>> actions) {
-        for (final Map.Entry<Integer, Class<? extends Activity>> entry : actions.entrySet()) {
-            findViewById(entry.getKey()).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, entry.getValue());
-                    startActivity(intent);
-                }
-            });
-        }
+    private <T extends Activity> void setListener(int buttonResId, final Class<T> activity) {
+        findViewById(buttonResId).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, activity));
+            }
+        });
     }
 }
