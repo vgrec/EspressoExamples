1b8db74727b02ab4557cc09a176a4c77cfc649d5
vgrec <vgrec@winify.com>
2015-03-24 10:44:10 +0200

    Removed unused menu files.

--- app/src/main/java/com/vgrec/espressoexamples/activities/DialogExampleActivity.java
+++ app/src/main/java/com/vgrec/espressoexamples/activities/DialogExampleActivity.java
@@ -15,26 +15,4 @@ public class DialogExampleActivity extends ActionBarActivity {
         setContentView(R.layout.activity_dialog_example);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dialog_example, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
