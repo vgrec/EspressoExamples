daeafcf3112f04607286d2628e0dbf80a09664fd
vgrec <vgrec@winify.com>
2015-03-30 14:38:00 +0300

    Move RecyclerView in xml

--- app/src/main/java/com/vgrec/espressoexamples/activities/RecyclerViewActivity.java
+++ app/src/main/java/com/vgrec/espressoexamples/activities/RecyclerViewActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.vgrec.espressoexamples.R;
 import com.vgrec.espressoexamples.adapter.RecyclerBooksAdapter;
 
 public class RecyclerViewActivity extends ActionBarActivity {
@@ -12,9 +13,9 @@ public class RecyclerViewActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        RecyclerView recyclerView = new RecyclerView(this);
+        setContentView(R.layout.activity_recycler_view);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new RecyclerBooksAdapter(this));
-        setContentView(recyclerView);
     }
 }
